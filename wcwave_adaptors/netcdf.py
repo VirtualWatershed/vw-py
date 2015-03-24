f3fb119724ece41f30aece60b450fd182b791838
Matthew Turner <maturner01@gmail.com>
2015-03-23 19:16:40 -0700

    iSNOBAL input .nc generated starting with CDL template with tests passing

--- wcwave_adaptors/netcdf.py
+++ wcwave_adaptors/netcdf.py
@@ -1,104 +1,109 @@
 """
 
 """
-import utm
+from jinja2 import Environment, FileSystemLoader
+from subprocess import Popen
+
 import numpy as np
 import netCDF4 as nc
+import os
+import uuid
+import utm
+
+from wcwave_adaptors.isnobal import IPW, VARNAME_DICT
 
 
-def ipwfile2netcdf(ipw_filename, ipw_type=None, netcdf=None):
-    """Convert or append an
-       `IPW iSNOBAL file <http://cgiss.boisestate.edu/~hpm/software/IPW/man1/isnobal.html>`_
-       to a NetCDF file.
+def ipw2nc(ipw_dir, nc_dir='./'):
+    """Aggregate all iSNOBAL files from ipw_dir into a single CF-NetCDF stored
+       in nc_dir.
 
-       Args:
-        ipw_filename (str) name of IPW file to convert to netcdf
-        ipw_type (str) one of init, mask, dem, ppt, in, em, or snow. Corresponds
-            to the iSNOBAL types of file inputs
-        netcdf (netCDF4.Dataset)
-
-    """
-    if not netcdf:
-        netcdf = _from_ipw_init()
-
-    # else:
-        # validate_ipw_netcdf(netcdf)
-
-    ipw = IPW(ipw_filename)
-
-    if not ipw_type:
-        ipw_type = 'in'
-
-    for var_label in VARNAME_DICT[ipw_type]:
-        # dem and mask images are time-independent
-        if ipw_type in ['dem', 'mask']:
-            var = netcdf.createVariable(var_label, np.float32, ('lat', 'lon'))
-            var[:] = ipw[var_label]
-
-        else:
-            var = netcdf.createVariable(var_label, np.float32,
-                                        ('time', 'lat', 'lon'))
-
-            var[:, :, :] = np.reshape(ipw[var_label],
-                                      (1, (ipw.nsamps, ipw.nlines)))
-
-
-def _from_ipw_init(start_time='2012-10-01 00:00:00', nc_filename=None, latlon_coords=None):
-    """Create a NetCDF that is CF-compliant and has all necessary dimensions for
-       ingesting an IPW file.
+        Arguments:
+            ipw_dir (str): directory where IPW files are stored with standard
+                structure
+            nc_dir (str): destination directory for NetCDF file
 
         Returns:
-            (netCDF4.Dataset) Dataset initialized with time, lat, and lon
-                              variables. Lat and lon filled in using
+            (NetCDF4.Dataset) NetCDF dataset for further manipulation if
+                desired
     """
-    nc_filename = nc_filename or 'new.nc'
-
-    netcdf = nc.Dataset(nc_filename, mode='wr', format='NETCDF4')
-
-    # time
-    time_dim = netcdf.createDimension('time', None)
-    time = ncfile.createVariable('time', np.float64, ('time',))
-    time.units = 'hours since ' + start_time
-    time.long_name = 'time'
-
-    lat_dim = netcdf.createDimension('lat', len(latlon_coords))
-    lon_dim = netcdf.createDimension('lon', len(latlon_coords))
-
-    lat = netcdf.createVariable('lat', np.float32, ('lat',))
-    lon = netcdf.createVariable('lon', np.float32, ('lon',))
-
-    if latlon_coords:
-        assert type(latlon_coords) is np.ndarray, \
-            "latlon_coords must be an np.ndarray"
-
-        lat[:] = latlon_coords[:, 0]
-        lon[:] = latlon_coords[:, 1]
-
-    return netcdf
+    pass
 
 
-def append_ipw_dem(ipw_dem, netcdf):
-    """Append an IPW dem to an existing netcdf file. Overwrites if one is
-       already present.
+def ncgen_from_template(template_filename, ncout_filename,
+                        cdl_output_filename=None, return_nc=True,
+                        overwrite=False,
+                        **kwargs):
+    """Generate a NetCDF file from a template"""
+    print os.path.join(os.path.dirname(__file__), 'cdl')
 
+    if not cdl_output_filename:
+        cdl_output_filename = '/tmp/' + uuid.uuid4() + '.cdl'
+
+    if os.path.isfile(cdl_output_filename) and not overwrite:
+        raise NCOError("CDL file %s already exists and overwrite is false" %
+                       cdl_output_filename)
+
+    _build_cdl(template_filename, cdl_output_filename, **kwargs)
+
+    nc = ncgen(cdl_output_filename, ncout_filename)
+
+    return nc
+
+
+CDL_TEMPLATE_ENV = Environment(loader=FileSystemLoader(
+                               os.path.join(os.path.dirname(__file__), 'cdl')))
+
+
+def _build_cdl(template_path, cdl_output_filename, **kwargs):
+    """Build a CDL for use in ncgen_from_template utility in conjunction with
+        ncgen wrapper.
+
+        (**kwargs) Template arguments appropriate for the type of template.
+            TODO this is not super sustainable. As models are added, we should
+            likely have Classes or some other data structure to represent the
+            required fields for, say, iSNOBAL input or output, or PRMS in/out
     """
-    alt = netcdf.createDimension('alt', np.float32, ('lat', 'lon'))
-    alt.standard_name = 'height'
-    alt.long_name = 'vertical distance above the surface'
-    alt.units = 'm'
-    alt.positive = 'up'
-    alt.axis = 'Z'
+    print "trying..."
+    template = CDL_TEMPLATE_ENV.get_template(template_path)
+    print
+    rendered = template.render(**kwargs)
+
+    if cdl_output_filename:
+        open(cdl_output_filename, 'w').write(rendered)
+
+    return rendered
 
 
-
-def make_latlon(bsamp=None, bline=None, dsamp=None, dline=None,
-                nsamp=None, nline=None):
+def ncgen(cdl_path, output_path, return_nc=True):
+    """Wrapper for the NCO tool of the same name. See
+        https://www.mankier.com/1/ncgen
     """
-    Create latitude and longitude variables based on the bline and bsamp
-    variables given.
+    p = Popen('ncgen -o %s %s' % (output_path, cdl_path), shell=True)
 
-        Returns:
-            2 x (nline * nsamp) array of lat/lon coordinates
+    retvals = p.communicate()
+
+    if p.returncode != 0:
+        raise NCOError(
+            "CDL file %s could not be made into a .nc with ncgen" % cdl_path
+            + "\nnco stderr: %s", retvals[1])
+
+    elif return_nc:
+        # return dataset in 'append' mode. NC4 all day ery day (default)
+        return nc.Dataset(output_path, 'a')
+
+
+class NCOError(Exception):
+    pass
+
+
+def utm2latlon(bsamp=None, bline=None, dsamp=None, dline=None,
+               nsamp=None, nline=None, utm_zone=11, utm_letter='T'):
+    """Create latitude and longitude variables based on the bline and bsamp
+       variables given. Default UTM zone is 11T, where Dry Crrek and Reynold's
+       Creek are.
+
+       Returns:
+           2 x (nline * nsamp) array of lat/lon coordinates
     """
     lines = [bline + dline*i for i in range(nline)]
     samps = [bsamp + dsamp*i for i in range(nsamp)]
@@ -106,4 +111,3 @@ def make_latlon(bsamp=None, bline=None, dsamp=None, dline=None,
     latlon_arr = [utm.to_latlon(s, l, 11, 'U') for s in samps for l in lines]
 
     return np.array(latlon_arr)
-
--- wcwave_adaptors/test/test_netcdf.py
+++ wcwave_adaptors/test/test_netcdf.py
@@ -2,10 +2,10 @@
    metadata gleaning, CF-Station data conversion, and maybe more, like opendap.
 """
 
+import os
 import unittest
 
-# from wcwave_adaptors.netcdf import *
-from ..netcdf import isCF
+from wcwave_adaptors.netcdf import utm2latlon, ncgen_from_template
 
 
 class TestNetCDF(unittest.TestCase):
@@ -13,12 +13,60 @@ class TestNetCDF(unittest.TestCase):
     """
 
     def setUp(self):
-        pass
+        self.cdl_build_path = 'wcwave_adaptors/test/test_cdl_path.cdl'
+        self.nc_out_path = 'wcwave_adaptors/test/test_nc_from_template.nc'
 
-    def test_cf_enforcement(self):
-        "Check CF standard breaking and meeting are properly detected"
-        assert False
+    def test_utm_latlon_conversion(self):
+        "check utm to latlon conversion of Dry Creek actually maps there"
+        # bbox corners from upper left clockwise around
+        east0 = 569029.6
+        east1 = 569452.1
+        north0 = 4842544.9
+        north1 = 4842177.4
 
-    def test_station_conversion(self):
-        "Properly convert compliant weather station csv to NetCDF Dataset"
-        assert False
+        utm_zone = 11
+        utm_letter = 'T'
+
+        latlons = utm2latlon(bsamp=east0, bline=north0, dsamp=east1-east0,
+                             dline=north1-north0, nsamp=2, nline=2,
+                             utm_zone=utm_zone, utm_letter=utm_letter)
+
+        from utm import to_latlon
+        ll0 = to_latlon(east0, north0, utm_zone, utm_letter)
+        ll1 = to_latlon(east1, north0, utm_zone, utm_letter)
+        ll2 = to_latlon(east1, north1, utm_zone, utm_letter)
+        ll3 = to_latlon(east0, north1, utm_zone, utm_letter)
+
+        assert (ll0 == latlons[0]).all
+        assert (ll1 == latlons[1]).all
+        assert (ll2 == latlons[2]).all
+        assert (ll3 == latlons[3]).all
+
+    def test_ncgen(self):
+        "CDL is generated from template and arguments and can build a valid NetCDF using `ncgen`"
+
+        # ncgen for iSNOBAL
+        template_path = 'ipw_in_template.cdl'
+
+        template_args = dict(bline=100, bsamp=10, dline=1.0, dsamp=-1.0,
+                             nlines=50, nsamps=10, dt=1, year=2010, month=10,
+                             day="01")
+
+        nc = ncgen_from_template(template_path, self.nc_out_path,
+                                 cdl_output_filename=self.cdl_build_path,
+                                 **template_args)
+
+        assert os.path.exists(self.cdl_build_path), \
+            "CDL template did not get built!"
+
+        assert os.path.exists(self.cdl_build_path), \
+            "NetCDF did not get built!"
+
+        assert nc, "NetCDF Python Dataset representation is None!"
+
+    def tearDown(self):
+        if os.path.exists(self.cdl_build_path):
+            os.remove(self.cdl_build_path)
+
+        if os.path.exists(self.nc_out_path):
+            os.remove(self.nc_out_path)
