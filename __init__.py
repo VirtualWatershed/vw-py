e7775c2d0ad6f0f354dc2803e4e19fd9c46c3dd0
Matthew Turner <maturner01@gmail.com>
2016-01-23 21:24:38 -0800

    removed grids import from __init__.py

--- __init__.py
+++ __init__.py
@@ -4,4 +4,3 @@ from vwpy.netcdf import ncgen_from_template, ncgen, utm2latlon
 from vwpy.watershed import (make_fgdc_metadata,
     make_watershed_metadata, VWClient, default_vw_client, metadata_from_file,
     QueryResult)
-from vwpy.grids import create_isnobal_dataset
