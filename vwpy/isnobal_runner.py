654a8746c07f2556aa2a7413f56593ee9cca4267
Moinul Hossain <moinul.cse@gmail.com>
2016-02-02 10:51:30 -0800

    just updated code style

--- vwpy/isnobal_runner.py
+++ vwpy/isnobal_runner.py
@@ -1,6 +1,6 @@
 from isnobal import isnobal
 import netCDF4
-def run_isnobal(input_path=None,output_path=None,event_emitter=None,**kwargs):
+def run_isnobal(input_path=None, output_path=None, event_emitter=None, **kwargs):
     input_nc = netCDF4.Dataset(input_path)
-    nc_out = isnobal(input_nc, output_path,event_emitter=event_emitter,**kwargs)
+    nc_out = isnobal(input_nc, output_path, event_emitter=event_emitter, **kwargs)
     nc_out.close()
