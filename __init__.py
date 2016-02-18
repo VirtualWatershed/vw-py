b1442995c1a6660c9fd01534c761ba177019c46f
Matthew Turner <maturner01@gmail.com>
2016-02-17 17:18:56 -0800

    added one more import __init__

--- __init__.py
+++ __init__.py
@@ -3,4 +3,4 @@ from vwpy.isnobal import (isnobal, IPW, generate_standard_nc,
 from vwpy.netcdf import ncgen_from_template, ncgen, utm2latlon
 from vwpy.watershed import (make_fgdc_metadata,
     make_watershed_metadata, VWClient, default_vw_client, metadata_from_file,
-    QueryResult)
+    QueryResult, _get_config)
