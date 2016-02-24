9537bf325d356a29b245c88462d51890518e3ddd
Matthew Turner <maturner01@gmail.com>
2016-02-23 16:13:52 -0800

    added public import of prms-to-netcdf adaptor

--- __init__.py
+++ __init__.py
@@ -4,3 +4,5 @@ from vwpy.netcdf import ncgen_from_template, ncgen, utm2latlon
 from vwpy.watershed import (make_fgdc_metadata,
     make_watershed_metadata, VWClient, default_vw_client, metadata_from_file,
     QueryResult)
+
+from vwpy.prms.text_to_netcdf.parameterToNetcdf import parameter_to_netcdf
